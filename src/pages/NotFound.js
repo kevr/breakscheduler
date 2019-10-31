7f17d3e0b50a0af911c5fcd5216c1760cf60c06a
Kevin Morris <kmorris@zerocost.dev>
2019-10-30 18:55:09 -0700

    Add more description to 404 page
    Signed-off-by: Kevin Morris <kmorris@zerocost.dev>

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Layout from './Layout';
 
 const NotFound = () => (
   <Layout pageTitle="Not Found">
-    <p className="text-center">404 - Not Found</p>
+    <p className="text-center"><h4>404 - Not Found</h4></p>
+    <p className="text-center">Visit <Link to="/">Home</Link> to find what you're looking for.</p>
   </Layout>
 );
 
