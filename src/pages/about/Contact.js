651eea4dd48f409a4ead1dc08076c8e52f960487
Kevin Morris <kmorris@zerocost.dev>
2019-11-06 20:20:08 -0800

    Fill out our 'Contact Us' page a bit more
    + src/config.json
        config.contact.email
    
    Signed-off-by: Kevin Morris <kmorris@zerocost.dev>

--- src/pages/about/Contact.js
+++ src/pages/about/Contact.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import Layout from '../Layout';
 import config from '../../config.json';
 
 const Contact = () => (
   <Layout pageTitle="Contact Us">
+
+    <div className="container">
+      <p className="Intro">
+        {`${config.appName} prides itself on it's ease-of-use and abundance
+        of topics which can be found in our `}<Link to="/help">Help</Link>
+        {` knowledgebase.`}
+      </p>
+    </div>
+
+    <div className="container">
+      <p className="Intro">
+        {`Need help with something not found in the knowledgebase? Have a
+          personal or business inquiry? For any other questions, contact
+          our team by sending an email to `}
+          <a href={`mailto:${config.contact.email}`}>{config.contact.email}</a>
+        {"."}
+      </p>
+    </div>
+
+    <div className="container">
+      <p className="Intro">
+        {`We look forward to hearing from you!`}
+      </p>
+    </div>
+
   </Layout>
 );
 
