59b2d1f57ed840f2f729f2fe21190767a21996e6
Kevin Morris <kmorris@zerocost.dev>
2019-11-28 23:56:25 -0800

    Add Session and Tickets reducers to Reducers
    Signed-off-by: Kevin Morris <kmorris@zerocost.dev>

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import Topics from './Topics';
+import Session from './Session';
+import Tickets from './Tickets';
 import { combineReducers } from 'redux';
 
 export default combineReducers({
-  topics: Topics
+  topics: Topics,
+  session: Session,
+  tickets: Tickets
 })
