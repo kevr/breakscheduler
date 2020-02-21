c58df6965e13a12d58bd43ca5a8d4646348009ff
Kevin Morris <kevr.gtalk@gmail.com>
2020-02-20 19:57:37 -0800

    Add Articles reducer
    * state.articles: {
        resolved: <reducer has been used with a specific command>,
        data: <article data array>
    }
    
    Signed-off-by: Kevin Morris <kevr.gtalk@gmail.com>

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import Topics from './Topics';
 import Session from './Session';
 import Tickets from './Tickets';
 import Message from './Message';
+import Articles from './Articles';
 import { combineReducers } from 'redux';
 
 export default combineReducers({
   session: Session,
   topics: Topics,
   tickets: Tickets,
-  message: Message
+  message: Message,
+  articles: Articles,
 });
